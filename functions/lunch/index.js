8476691f435f42f1bc48008c48b686ed7ff93c68
Kyo Yamada <hysteric.madam@gmail.com>
2020-07-26 22:01:30 +0900

    add: display "Yey!, Puff!" when put url parameter like ?text=Yey!

--- functions/lunch/index.js
+++ functions/lunch/index.js
@@ -1,4 +1,24 @@
-exports.puffLunch = (req, res) => {
-  let body = req.query.text;
-  res.send('Hello, text = ' + body);
+exports.function = async (req, res) => {
+
+  const verifyWebhook = (req) => {
+    let text = req.query.text;
+    console.log(text);
+    res.send(`${text}, Puff!`);
+  }
+
+  try {
+    if (req.method !== 'GET') {
+      const error = new Error('Only POST requests are accepted');
+      error.code = 405;
+      throw error;
+    }
+
+    verifyWebhook(req);
+
+  } catch (err) {
+    console.error(err);
+    res.status(err.code || 500).send(err);
+    return Promise.reject(err);
+  }
 };
+
