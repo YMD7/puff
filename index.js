dd9841633c823e54b3386cbbd0016ea264989cf6
Kyo Yamada <hysteric.madam@gmail.com>
2020-08-23 23:07:56 +0900

    add: dotenv

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
 exports.function = async (req, res) => {
   try {
     if (req.method !== 'GET') {
