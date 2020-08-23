8de0c35ad1e66671b4537d1c70b6b28b8eda4fa3
Kyo Yamada <hysteric.madam@gmail.com>
2020-08-23 22:50:57 +0900

    revise: change folder structure

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,10 @@
 exports.function = async (req, res) => {
-
-  const verifyWebhook = (req) => {
-    let text = req.query.text;
-    console.log(text);
-    res.send(`${text}, Puff!`);
-  }
-
   try {
     if (req.method !== 'GET') {
       const error = new Error('Only POST requests are accepted');
       error.code = 405;
       throw error;
     }
-
-    verifyWebhook(req);
-
   } catch (err) {
     console.error(err);
     res.status(err.code || 500).send(err);
