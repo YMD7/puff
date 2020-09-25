56ef75c00e608df75a0bb794c853e6b0e76c7542
Kyo Yamada <hysteric.madam@gmail.com>
2020-09-25 21:24:04 +0900

    install: javascript standard style, remove airbnb

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
+require('dotenv').config()
+const { App } = require('@slack/bolt')
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+})
 
 exports.function = async (req, res) => {
   try {
     if (req.method !== 'POST') {
-      const error = new Error('Only POST requests are accepted');
-      error.code = 405;
-      throw error;
+      const error = new Error('Only POST requests are accepted')
+      error.code = 405
+      throw error
     }
 
     const response = {
-      "challenge": req.body.challenge
-    };
-    res.json(response);
+      challenge: req.body.challenge
+    }
+    res.json(response)
 
-    return Promise.resolve();
+    return Promise.resolve()
   } catch (err) {
-    console.error(err);
-    res.status(err.code || 500).send(err);
-    return Promise.reject(err);
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
   }
-};
-
+}
